2509594f172dcd33c68352beddcc50b9c0fee411
Victor Pacheco <106631198+VkTRPacheco@users.noreply.github.com>
2023-08-24 16:32:30 -0300

    Update index.js

--- ws/index.js
+++ ws/index.js
@@ -2,10 +2,15 @@ const  express =  require('express');
 const app = express();
 const morgan = require('morgan');
 
+
+//Middlewares
 app.use(morgan('dev'));
 
+
+//Variaveis
 app.set('port', 8000);
 
 app.listen(app.get('port'), () => {
     console.log(`WS Escutando na porta ${app.get('port')}`);
-})
+
+});
